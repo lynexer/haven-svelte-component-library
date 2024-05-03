a00c4c5646c33b828f835933e28e5fdba15882f1
Kyle Graham <kylegraham05@gmail.com>
2024-05-03 06:42:30 -0700

    Add export for Ipad component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,5 @@ import Icon from './svelte/Icon.svelte';
 import type { IconProp } from './types/icon.js';
 
 export { Icon, type IconProp };
+
+export { default as Ipad } from './svelte/devices/Ipad.svelte';
