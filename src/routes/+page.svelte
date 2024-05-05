c70dd9499e811242ebd4da4595eb965fcdb16037
Kyle Graham <kylegraham05@gmail.com>
2024-05-05 14:40:55 -0700

    Create Spinner Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import Badge from '$lib/svelte/BadgeComponent.svelte';
+    import Spinner from '$lib/svelte/Spinner.svelte';
     import Ipad from '$lib/svelte/devices/Ipad.svelte';
 </script>
 
 <Ipad landscape size="xl" colour="silver" year="2018">
     <div class="text-white bg-gray-300 h-full">
         <Badge colour="warning">Test</Badge>
+        <Spinner size="base" colour="pink" />
     </div>
 </Ipad>
