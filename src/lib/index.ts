76610977640cd05a6e2d3195201abe63c2cbe8d5
Kyle Graham <kylegraham05@gmail.com>
2024-05-05 13:53:37 -0700

    Export badge component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import Icon from './svelte/Icon.svelte';
+import Badge from './svelte/BadgeComponent.svelte';
 import Ipad from './svelte/devices/Ipad.svelte';
 import type { IconProp } from './types/icon.js';
 
 export default Ipad;
-export { Icon, Ipad, type IconProp }
+export { Icon, Badge, Ipad, type IconProp }
