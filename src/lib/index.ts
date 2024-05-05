1e998fad5ce3f6393458350ed75988495dc14943
Kyle Graham <kylegraham05@gmail.com>
2024-05-05 14:41:36 -0700

    Export Spinner component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
 import Icon from './svelte/Icon.svelte';
 import Badge from './svelte/BadgeComponent.svelte';
+import Spinner from './svelte/Spinner.svelte';
 import Ipad from './svelte/devices/Ipad.svelte';
 import type { IconProp } from './types/icon.js';
 
 export default Ipad;
-export { Icon, Badge, Ipad, type IconProp }
+export { Icon, Badge, Spinner, Ipad, type IconProp }
