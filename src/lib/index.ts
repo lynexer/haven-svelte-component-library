9501ffb5e00119ba38762377e9fbffbc770e2f42
Kyle Graham <kylegraham05@gmail.com>
2024-05-07 17:03:13 -0700

    Update svelte component exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,13 @@
-import Icon from './svelte/Icon.svelte';
-import Badge from './svelte/BadgeComponent.svelte';
-import Spinner from './svelte/Spinner.svelte';
-import Ipad from './svelte/devices/Ipad.svelte';
-import PixelXl from './svelte/devices/PixelXl.svelte';
-import type { IconProp } from './types/icon.js';
+// Basic Components
+export { default as Icon } from './svelte/Icon.svelte';
+export { default as Badge } from './svelte/BadgeComponent.svelte';
+export { default as Spinner } from './svelte/Spinner.svelte';
 
-export { Icon, Badge, Spinner, Ipad, PixelXl, type IconProp }
+// Devices
+export { default as Ipad } from './svelte/devices/Ipad.svelte';
+export { default as PixelXl } from './svelte/devices/PixelXl.svelte';
+
+// Types
+export * from './types/badge.js';
+export * from './types/device.js';
+export * from './types/icon.js';
