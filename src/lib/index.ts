3b2f7118d7689ab380fc705f7522e1510026407f
Kyle Graham <kylegraham05@gmail.com>
2024-05-10 19:44:27 -0700

    Export input component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@ export { default as Icon } from './svelte/Icon.svelte';
 export { default as Badge } from './svelte/BadgeComponent.svelte';
 export { default as Spinner } from './svelte/Spinner.svelte';
 export { default as Pill } from './svelte/Pill.svelte';
+export { default as Input } from './svelte/Input.svelte';
 
 // Devices
 export { default as Ipad } from './svelte/devices/Ipad.svelte';
