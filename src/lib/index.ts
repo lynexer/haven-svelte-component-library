1f6a2098e3b4fa7d64b3675a53bcd639c3413b27
Kyle Graham <kylegraham05@gmail.com>
2024-06-21 21:40:59 -0700

    Create Kbd component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,6 +4,7 @@ export { default as Badge } from './svelte/BadgeComponent.svelte';
 export { default as Spinner } from './svelte/Spinner.svelte';
 export { default as Pill } from './svelte/Pill.svelte';
 export { default as Input } from './svelte/Input.svelte';
+export { default as Kbd } from './svelte/Kbd.svelte';
 
 // Devices
 export { default as Ipad } from './svelte/devices/Ipad.svelte';
---
+++ src/lib/svelte/Kbd.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { match } from "ts-pattern";
+
+    type Modes = 'light' | 'dark';
+
+    let className: string = '';
+
+    export let mode: Modes = 'dark';
+    export { className as class };
+
+    let getCssColours = (): string => {
+        return match(mode)
+            .returnType<string>()
+            .with('light', () => 'bg-white border-gray-200 text-gray-800 shadow-[0px_2px_0px_0px_rgba(0,0,0,0.08)]')
+            .otherwise(() => 'bg-neutral-900 border-neutral-700 text-neutral-200 shadow-[0px_2px_0px_0px_rgba(255,255,255,0.1)]');
+    };
+</script>
+
+<kbd class="min-h-[30px] inline-flex justify-center items-center py-1 px-1.5 border font-mono text-sm rounded-md {getCssColours} {className}">
+    <slot />
+</kbd>
